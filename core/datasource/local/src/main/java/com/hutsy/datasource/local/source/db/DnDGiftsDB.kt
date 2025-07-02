15727f2c2836fe1d451aaaeae458cb065e66cc74
ericwafula <ericwathome007@gmail.com>
2025-07-02 20:04:17 +0300

    added temporary mock db for initial project builds

--- core/datasource/local/src/main/java/com/hutsy/datasource/local/source/db/DnDGiftsDB.kt
+++ core/datasource/local/src/main/java/com/hutsy/datasource/local/source/db/DnDGiftsDB.kt
@@ -2,9 +2,12 @@ package com.hutsy.datasource.local.source.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.hutsy.datasource.local.source.db.mock.MockDb
 
 @Database(
-    entities = [],
+    entities = [
+        MockDb::class
+    ],
     version = 1,
     autoMigrations = [],
 )
